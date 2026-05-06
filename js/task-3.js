021b6e9c6c726547a9c4cdb74c53c3e41671c211
Anastasiia Kalyniuk <Kalyniuk.an@gmail.com>
2026-05-06 15:54:45 +0300

    apd task-3

--- js/task-3.js
+++ js/task-3.js
@@ -2,5 +2,7 @@ const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
 nameInput.addEventListener('input', () => {
-  nameOutput.textContent = nameInput.value.trim() === '' ? 'Anonymous' : nameInput.value;
+  const name = nameInput.value.trim();
+  nameOutput.textContent = name === '' ? 'Anonymous' : name;
+  // nameOutput.textContent = nameInput.value.trim() === '' ? 'Anonymous' : nameInput.value;
 });
